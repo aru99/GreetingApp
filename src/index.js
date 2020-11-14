bdba0c0d399f835fc807f553340c212da5f8d392
aru99 <mohammadarmanmavric@gmail.com>
2020-11-14 19:29:31 +0530

    Added the condition flow for the greeting message

--- src/index.js
+++ src/index.js
@@ -2,17 +2,28 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-// calculating the current time
+// ---------------calculating the current time------------------
 const curDate = new Date();
 const curTime = curDate.getHours();
 
-//greeting message decision
+//-----------------greeting message decision---------------------
+let greeting = " ";
+//conditional for time
+if (curTime >= 1 && curTime < 12) {
+  greeting = "Good Morning";
+} else if (curTime >= 12 && curTime < 16) {
+  greeting = "Good Afternoon";
+} else if (curTime >= 16 && curTime < 23) {
+  greeting = "Good Night";
+} else {
+  greeting = "not defined";
+}
 
 ReactDOM.render(
   //
   <>
     <div>
-      <h1>Hello sir, {curTime}</h1>
+      <h1>Hello sir, {greeting}</h1>
     </div>
   </>,
   document.getElementById("root")
