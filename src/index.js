24017dc65206775f2f2deb349944032eb9307638
aru99 <mohammadarmanmavric@gmail.com>
2020-11-14 21:14:23 +0530

    added styling, project finished

--- src/index.js
+++ src/index.js
@@ -6,15 +6,22 @@ import "./index.css";
 const curDate = new Date();
 const curTime = curDate.getHours();
 
+//-----------------cssStyling--------------------------
+
+const cssStyle = {};
+
 //-----------------greeting message decision---------------------
 let greeting = " ";
 //conditional for time
 if (curTime >= 1 && curTime < 12) {
   greeting = "Good Morning";
+  cssStyle.color = "Green";
 } else if (curTime >= 12 && curTime < 16) {
   greeting = "Good Afternoon";
+  cssStyle.color = "Orange";
 } else if (curTime >= 16 && curTime < 23) {
   greeting = "Good Night";
+  cssStyle.color = "Grey";
 } else {
   greeting = "not defined";
 }
@@ -23,7 +30,9 @@ ReactDOM.render(
   //
   <>
     <div>
-      <h1>Hello sir, {greeting}</h1>
+      <h1>
+        Hello sir, <span style={cssStyle}>{greeting}</span>{" "}
+      </h1>
     </div>
   </>,
   document.getElementById("root")
