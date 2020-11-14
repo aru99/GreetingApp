b55830fd9525f25d72251927998fec67d6d113d9
aru99 <mohammadarmanmavric@gmail.com>
2020-11-14 19:15:44 +0530

    acquired current date and time

--- src/index.js
+++ src/index.js
@@ -2,7 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-ReactDOM
-  .render
+// calculating the current time
+const curDate = new Date();
+const curTime = curDate.getHours();
+
+//greeting message decision
+
+ReactDOM.render(
   //
-  ();
+  <>
+    <div>
+      <h1>Hello sir, {curTime}</h1>
+    </div>
+  </>,
+  document.getElementById("root")
+);
